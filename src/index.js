37a5585e69054c0ef4a764ea442e07411fccdae6
ccw7259 <charles.wu@yumchina.com>
2023-04-25 20:30:31 +0800

    feat: ti

--- src/index.js
+++ src/index.js
@@ -1506,7 +1506,7 @@ var multiply = function (num1, num2) {
 };
 console.log(multiply("123456789",
   "987654321"))
-*/
+
 
 
 
@@ -1538,33 +1538,8 @@ var isMatch = function (s, p) {
 };
 
 console.log(isMatch('acdcb', 'a*c?b'))
+*/
 
-
-/**
- * 
- * 
- * 给你一个输入字符串 (s) 和一个字符模式 (p) ，请你实现一个支持 '?' 和 '*' 匹配规则的通配符匹配：
-'?' 可以匹配任何单个字符。
-'*' 可以匹配任意字符序列（包括空字符序列）。
-判定匹配成功的充要条件是：字符模式必须能够 完全匹配 输入字符串（而不是部分匹配）。
-
-
- * 输入：s = "aa", p = "a"
-输出：false
-解释："a" 无法匹配 "aa" 整个字符串。
-
-输入：s = "aa", p = "*"
-输出：true
-解释：'*' 可以匹配任意字符串。
-
-
-输入：s = "cb", p = "?a"
-输出：false
-解释：'?' 可以匹配 'c', 但第二个 'a' 无法匹配 'b'。
-
-"acdcb"
-"a*c?b"
- */
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
