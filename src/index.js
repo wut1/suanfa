a3806de716d47056cf1b978dc24b2d8cd7fd51c8
ccw7259 <charles.wu@yumchina.com>
2023-05-08 15:28:42 +0800

    feat(难): N皇后

--- src/index.js
+++ src/index.js
@@ -1676,7 +1676,7 @@ var groupAnagrams = function (strs) {
 };
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
-*/
+
 
 
 
@@ -1721,6 +1721,72 @@ var myPow = function (x, n) {
 };
 
 console.log(myPow(8.88023, 3))
+*/
+
+
+var solveNQueens = function (n) {
+
+
+  function isValid(row, col, chessBoard) {
+
+    for (let i = 0; i < row; i++) {
+      if (chessBoard[i][col] === 'Q') {
+        return false
+      }
+    }
+
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (chessBoard[i][j] === 'Q') {
+        return false
+      }
+    }
+
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (chessBoard[i][j] === 'Q') {
+        return false
+      }
+    }
+    return true
+  }
+
+  function transformChessBoard(chessBoard) {
+    let chessBoardBack = []
+    chessBoard.forEach(row => {
+      let rowStr = ''
+      row.forEach(value => {
+        rowStr += value
+      })
+      chessBoardBack.push(rowStr)
+    })
+
+    return chessBoardBack
+  }
+
+  let result = []
+  function backtracing(row, chessBoard) {
+    if (row === n) {
+  
+      result.push(transformChessBoard(chessBoard))
+     
+      return
+    }
+    for (let col = 0; col < n; col++) {
+      if (isValid(row, col, chessBoard)) {
+        chessBoard[row][col] = 'Q'
+        backtracing(row + 1, chessBoard)
+        chessBoard[row][col] = '.'
+      }
+    }
+  }
+  let chessBoard = new Array(n).fill([]).map(() => new Array(n).fill('.'))
+  backtracing(0, chessBoard)
+  return result
+};
+console.log(solveNQueens(4))
+/**
+ * 输入：n = 4
+输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
